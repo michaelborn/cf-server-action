08ae5d32a6505fc847e659387bd1f3244106708a
Michael Born <michael@ortussolutions.com>
2022-02-01 07:58:14 -0500

    🐛 FIX: Try pulling image first, upping the sleep time

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,11 @@ DOCKER_CONTAINER=ortussolutions/commandbox
 # Important to set/override this for proper port mapping.
 export BOX_SERVER_WEB_HTTP_PORT=8080
 
+echo "Pulling latest $DOCKER_CONTAINER:$IMAGE_TAG image"
+docker pull $DOCKER_CONTAINER:$IMAGE_TAG
+
 echo "Starting commandbox server instance"
 echo "docker run --detach --publish $SERVER_PORT:8080 --volume $PWD:/app $DOCKER_CONTAINER:$IMAGE_TAG"
-
 docker run \
     --publish $SERVER_PORT:8080 \
     --env PORT=8080 \
@@ -20,6 +22,6 @@ docker run \
     --detach \
     $DOCKER_CONTAINER:$IMAGE_TAG
 
-sleep 5
+sleep 30
 
 docker run --rm curlimages/curl:7.81.0 http://127.0.0.1:$SERVER_PORT/index.cfm
